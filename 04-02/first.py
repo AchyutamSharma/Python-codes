22e1bcaf6ee02b678dd37020c60041a13bd1f8c1
Akshutam Sharma <111337963+AchyutamSharma@users.noreply.github.com>
2024-02-05 16:57:02 +0530

    Add files via upload

---
+++ 04-02/first.py
@@ -0,0 +1,13 @@
+#Hey, this is comment
+""" 
+This is Multi-line comment                                                                             Date - 04/02/24
+Today, I started 60days of code of Python
+"""
+
+print("Today is my first day of \"Python\" \n \t What I learn today :- \n\t\t1. How to give comment in python...")
+print("\t\t\t Multi-line :- \"\"\"Comments\"\"\"  Or    '''This is also''' \n\t\t\t Single-line :- #Comments")
+print("\t\t2. How to give Escape sequence is work.\n\t\t3. How to use pip and module(import) in terminal ")
+print('\t\t4. What is \'sep\' and \'end\' keyword:- \n\t\t\t Example',6,7, sep="-^", end=' This is separator, and this is \'end\' up with this... ')
+
+
+
