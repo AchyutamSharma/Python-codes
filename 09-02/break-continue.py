38fe77d96f841c305fee7c157f0f3a4e0831934d
Akshutam Sharma <111337963+AchyutamSharma@users.noreply.github.com>
2024-02-10 11:45:19 +0530

    Add files via upload

---
+++ 09-02/break-continue.py
@@ -0,0 +1,6 @@
+
+tableof = int(input('Enter the Number of table you want :'))
+length = int(input('Enter the length of multipilcation table : '))
+
+for i in range(length):
+    print()
---
+++ 09-02/while-loop.py
@@ -0,0 +1,13 @@
+
+a = int(input("Enter the number : "))
+i = 1
+print('This is 1 to n')
+while(i<=a):
+    print(i)
+    i+=1
+
+print("\n\tThis is n to 1\n")
+i = a
+while(i > 0):
+    print(i)
+    i-=1
