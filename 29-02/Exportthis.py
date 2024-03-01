cc6d993335067b738d4a92a22af1c3186872acb0
Akshutam Sharma <111337963+AchyutamSharma@users.noreply.github.com>
2024-03-01 14:37:56 +0530

    Add files via upload

---
+++ 29-02/Exportthis.py
@@ -0,0 +1,5 @@
+
+def welcome():
+    print("Hello there, Welcome to our website.")
+
+its_me = "Its me Akshutam. I'm your developer."
---
+++ 29-02/inside.py
@@ -0,0 +1,22 @@
+import math
+from Exportthis import *
+
+# from math import pi, sqrt,pow,floor as s
+
+import math as m
+
+
+welcome()
+print(its_me)
+
+print("This is how import works.")
+
+
+print("This is floor = ",m.floor(499.515))
+
+squrts = m.sqrt(5)
+print("This is Square root of 5 = ",squrts)
+
+powers = m.pow(5,2)
+print("This is pow of 5 = ",powers)
+
