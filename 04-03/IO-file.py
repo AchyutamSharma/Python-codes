79deb7f3fee84f03b83edef12560b331721a227b
Akshutam Sharma <111337963+AchyutamSharma@users.noreply.github.com>
2024-03-13 18:42:36 +0530

    Add files via upload

---
+++ 04-03/IO-file.py
@@ -0,0 +1,25 @@
+
+# Reading File.
+
+
+# f = open("demo.txt","r")
+# print(f.read())
+# f.close()
+
+
+
+# Writting using append 'a' 
+# it add txt at end line of your file 
+
+# of = open("demo.txt","a") 
+# of.write('Hello, add using write function.')
+# of.close()
+
+
+# write 
+#it delete all the content of your file and add new one 
+
+# of = open("demo.txt","w")
+# of.write('Hello, add using write function.')
+# of.close()
+
